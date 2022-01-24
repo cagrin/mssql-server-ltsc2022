27d1aa258e6840d079e4ab7f9c0ba3b29509e430
Krzysztof Strugiński <cagrin@gmail.com>
2022-01-25 00:25:38 +0100

    Add ARG SQLCOLLATION

--- developer/build.ps1
+++ developer/build.ps1
@@ -1,20 +1,20 @@
 cd 2012-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2012-latest --file dockerfile .
-docker build -m 4g -t cagrin/mssql-server-oldies:2012-latest-pl --file dockerfile_pl .
+docker build -m 4g -t cagrin/mssql-server-oldies:2012-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
+docker build -m 4g -t cagrin/mssql-server-oldies:2012-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
 cd..
 cd 2014-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2014-latest --file dockerfile .
-docker build -m 4g -t cagrin/mssql-server-oldies:2014-latest-pl --file dockerfile_pl .
+docker build -m 4g -t cagrin/mssql-server-oldies:2014-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
+docker build -m 4g -t cagrin/mssql-server-oldies:2014-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
 cd..
 cd 2016-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2016-latest --file dockerfile .
-docker build -m 4g -t cagrin/mssql-server-oldies:2016-latest-pl --file dockerfile_pl .
+docker build -m 4g -t cagrin/mssql-server-oldies:2016-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
+docker build -m 4g -t cagrin/mssql-server-oldies:2016-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
 cd..
 cd 2017-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2017-latest --file dockerfile .
-docker build -m 4g -t cagrin/mssql-server-oldies:2017-latest-pl --file dockerfile_pl .
+docker build -m 4g -t cagrin/mssql-server-oldies:2017-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
+docker build -m 4g -t cagrin/mssql-server-oldies:2017-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
 cd..
 cd 2019-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2019-latest --file dockerfile .
-docker build -m 4g -t cagrin/mssql-server-oldies:2019-latest-pl --file dockerfile_pl .
+docker build -m 4g -t cagrin/mssql-server-oldies:2019-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
+docker build -m 4g -t cagrin/mssql-server-oldies:2019-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
 cd..
