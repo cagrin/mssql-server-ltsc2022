b6fb58fe48fa85af54b27d7d12f025670061543b
Krzysztof Strugiński <cagrin@gmail.com>
2022-01-25 21:43:12 +0100

    Add Build-Image and Test-Image functions

--- developer/build.ps1
+++ developer/build.ps1
@@ -1,20 +1,23 @@
-cd 2012-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2012-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
-docker build -m 4g -t cagrin/mssql-server-oldies:2012-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
-cd..
-cd 2014-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2014-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
-docker build -m 4g -t cagrin/mssql-server-oldies:2014-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
-cd..
-cd 2016-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2016-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
-docker build -m 4g -t cagrin/mssql-server-oldies:2016-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
-cd..
-cd 2017-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2017-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
-docker build -m 4g -t cagrin/mssql-server-oldies:2017-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
-cd..
-cd 2019-latest
-docker build -m 4g -t cagrin/mssql-server-oldies:2019-latest --build-arg SQLCOLLATION=SQL_Latin1_General_CP1_CI_AS .
-docker build -m 4g -t cagrin/mssql-server-oldies:2019-latest-pl --build-arg SQLCOLLATION=Polish_CI_AS .
-cd..
+function Build-Image
+{
+    param
+    (
+        [string]$Tag,
+        [string]$WorkingFolder,
+        [string]$Collation
+    )
+
+    docker build -m 4g -t cagrin/mssql-server-oldies:$Tag --build-arg SQLCOLLATION=$Collation ./$WorkingFolder
+}
+
+Build-Image '2012-latest' '2012-latest' 'SQL_Latin1_General_CP1_CI_AS'
+Build-Image '2014-latest' '2014-latest' 'SQL_Latin1_General_CP1_CI_AS'
+Build-Image '2016-latest' '2016-latest' 'SQL_Latin1_General_CP1_CI_AS'
+Build-Image '2017-latest' '2017-latest' 'SQL_Latin1_General_CP1_CI_AS'
+Build-Image '2019-latest' '2019-latest' 'SQL_Latin1_General_CP1_CI_AS'
+
+Build-Image '2012-latest-pl' '2012-latest' 'Polish_CI_AS'
+Build-Image '2014-latest-pl' '2014-latest' 'Polish_CI_AS'
+Build-Image '2016-latest-pl' '2016-latest' 'Polish_CI_AS'
+Build-Image '2017-latest-pl' '2017-latest' 'Polish_CI_AS'
+Build-Image '2019-latest-pl' '2019-latest' 'Polish_CI_AS'
---
+++ developer/test.ps1
@@ -0,0 +1,33 @@
+function Test-Image
+{
+    param
+    (
+        [string]$Tag,
+        [string]$ProductVersion,
+        [string]$Collation
+    )
+
+    Write-Host "Test-Image '$Tag' '$ProductVersion' '$Collation'" -ForegroundColor Magenta
+    $SA_PASSWORD='A.794613'
+    docker container stop test_image
+    docker container rm test_image
+    docker run --name test_image -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" -p 51433:1433 -m 4g -d cagrin/mssql-server-oldies:$Tag
+    Start-Sleep -Second 10
+    Invoke-Sqlcmd -ServerInstance "localhost,51433" -Database "master" -Username "sa" -Password "$SA_PASSWORD" -Query "IF SERVERPROPERTY('ProductVersion') <> '$ProductVersion' RAISERROR ('ProductVersion is invalid', 16, 1)"
+    Invoke-Sqlcmd -ServerInstance "localhost,51433" -Database "master" -Username "sa" -Password "$SA_PASSWORD" -Query "IF SERVERPROPERTY('Collation') <> '$Collation' RAISERROR ('Collation is invalid', 16, 1)"
+}
+
+Test-Image '2012-latest' '11.0.7507.2' 'SQL_Latin1_General_CP1_CI_AS'
+Test-Image '2014-latest' '12.0.6433.1' 'SQL_Latin1_General_CP1_CI_AS'
+Test-Image '2016-latest' '13.0.6404.1' 'SQL_Latin1_General_CP1_CI_AS'
+Test-Image '2017-latest' '14.0.3430.2' 'SQL_Latin1_General_CP1_CI_AS'
+Test-Image '2019-latest' '15.0.4188.2' 'SQL_Latin1_General_CP1_CI_AS'
+
+Test-Image '2012-latest-pl' '11.0.7507.2' 'Polish_CI_AS'
+Test-Image '2014-latest-pl' '12.0.6433.1' 'Polish_CI_AS'
+Test-Image '2016-latest-pl' '13.0.6404.1' 'Polish_CI_AS'
+Test-Image '2017-latest-pl' '14.0.3430.2' 'Polish_CI_AS'
+Test-Image '2019-latest-pl' '15.0.4188.2' 'Polish_CI_AS'
+
+docker container stop test_image
+docker container rm test_image
