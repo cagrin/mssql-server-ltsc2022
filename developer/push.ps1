3c78af054f0ec988f3d6db9558187267fda9525f
Krzysztof Strugiński <cagrin@gmail.com>
2022-01-26 00:49:42 +0100

    Update Push-Image function

--- developer/push.ps1
+++ developer/push.ps1
@@ -1,10 +1,11 @@
 docker push cagrin/mssql-server-oldies:2012-latest
-docker push cagrin/mssql-server-oldies:2012-latest-pl
 docker push cagrin/mssql-server-oldies:2014-latest
-docker push cagrin/mssql-server-oldies:2014-latest-pl
 docker push cagrin/mssql-server-oldies:2016-latest
-docker push cagrin/mssql-server-oldies:2016-latest-pl
 docker push cagrin/mssql-server-oldies:2017-latest
-docker push cagrin/mssql-server-oldies:2017-latest-pl
 docker push cagrin/mssql-server-oldies:2019-latest
+
+docker push cagrin/mssql-server-oldies:2012-latest-pl
+docker push cagrin/mssql-server-oldies:2014-latest-pl
+docker push cagrin/mssql-server-oldies:2016-latest-pl
+docker push cagrin/mssql-server-oldies:2017-latest-pl
 docker push cagrin/mssql-server-oldies:2019-latest-pl
