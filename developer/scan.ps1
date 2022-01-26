55bae6c430d45fd938212c2e95d89e5bbb79f9be
Krzysztof Strugiński <cagrin@gmail.com>
2022-01-27 00:49:49 +0100

    Add scan script

---
+++ developer/scan.ps1
@@ -0,0 +1,11 @@
+docker scan cagrin/mssql-server-oldies:2012-latest
+docker scan cagrin/mssql-server-oldies:2014-latest
+docker scan cagrin/mssql-server-oldies:2016-latest
+docker scan cagrin/mssql-server-oldies:2017-latest
+docker scan cagrin/mssql-server-oldies:2019-latest
+
+docker scan cagrin/mssql-server-oldies:2012-latest-pl
+docker scan cagrin/mssql-server-oldies:2014-latest-pl
+docker scan cagrin/mssql-server-oldies:2016-latest-pl
+docker scan cagrin/mssql-server-oldies:2017-latest-pl
+docker scan cagrin/mssql-server-oldies:2019-latest-pl
