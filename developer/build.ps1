600db0debdca6a36c9aea64f6a230455a7d513b6
Krzysztof Strugiński <80916480+cagrin@users.noreply.github.com>
2023-03-28 11:42:04 +0000

    Add experimental online build images

--- developer/build.ps1
+++ developer/build.ps1
@@ -7,8 +7,7 @@ function Build-Image
         [string]$Collation
     )
 
-    $IP = (Get-NetIPAddress -PrefixOrigin Dhcp).IPAddress | Select-Object -First 1
-    docker build --memory 4g --tag cagrin/mssql-server-ltsc2022:$Tag --build-arg SQLCOLLATION=$Collation --build-arg SHARE_PATH="\\$IP\Public" --build-arg SHARE_LOGIN="sa" --build-arg SHARE_PASSWORD="A.794613" ./$WorkingFolder
+    docker build --memory 4g --tag cagrin/mssql-server-ltsc2022:$Tag --build-arg SQLCOLLATION=$Collation ./$WorkingFolder
 }
 
 function Build-Version
@@ -19,13 +18,13 @@ function Build-Version
     )
 
     Build-Image $WorkingFolder      $WorkingFolder 'SQL_Latin1_General_CP1_CI_AS'
-    Build-Image $WorkingFolder'-pl' $WorkingFolder 'Polish_CI_AS'
-    Build-Image $WorkingFolder'-ua' $WorkingFolder 'Ukrainian_CI_AS'
+    #Build-Image $WorkingFolder'-pl' $WorkingFolder 'Polish_CI_AS'
+    #Build-Image $WorkingFolder'-ua' $WorkingFolder 'Ukrainian_CI_AS'
 }
 
-Build-Version '2012-latest'
-Build-Version '2014-latest'
-Build-Version '2016-latest'
-Build-Version '2017-latest'
-Build-Version '2019-latest'
+#Build-Version '2012-latest'
+#Build-Version '2014-latest'
+#Build-Version '2016-latest'
+#Build-Version '2017-latest'
+#Build-Version '2019-latest'
 Build-Version '2022-latest'
--- developer/test.ps1
+++ developer/test.ps1
@@ -9,12 +9,12 @@ function Test-Image
 
     Write-Host "Test-Image '$Tag' '$ProductVersion' '$Collation'" -ForegroundColor Magenta
     $MSSQL_SA_PASSWORD='A.794613'
-    docker container stop test_image
-    docker container rm test_image
     docker run --name test_image --memory 4g -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD" -p 51433:1433 -d cagrin/mssql-server-ltsc2022:$Tag
     Start-Sleep -Second 15
     Invoke-Sqlcmd -ServerInstance "localhost,51433" -Database "master" -Username "sa" -Password "$MSSQL_SA_PASSWORD" -Query "IF SERVERPROPERTY('ProductVersion') <> '$ProductVersion' RAISERROR ('ProductVersion is invalid', 16, 1)"
     Invoke-Sqlcmd -ServerInstance "localhost,51433" -Database "master" -Username "sa" -Password "$MSSQL_SA_PASSWORD" -Query "IF SERVERPROPERTY('Collation') <> '$Collation' RAISERROR ('Collation is invalid', 16, 1)"
+    docker container stop test_image
+    docker container rm test_image
 }
 
 function Test-Version
@@ -26,16 +26,13 @@ function Test-Version
     )
 
     Test-Image $Tag $ProductVersion 'SQL_Latin1_General_CP1_CI_AS'
-    Test-Image $Tag'-pl' $ProductVersion 'Polish_CI_AS'
-    Test-Image $Tag'-ua' $ProductVersion 'Ukrainian_CI_AS'
-
-    docker container stop test_image
-    docker container rm test_image
+    #Test-Image $Tag'-pl' $ProductVersion 'Polish_CI_AS'
+    #Test-Image $Tag'-ua' $ProductVersion 'Ukrainian_CI_AS'
 }
 
-Test-Version '2012-latest' '11.0.7507.2'
-Test-Version '2014-latest' '12.0.6444.4'
-Test-Version '2016-latest' '13.0.7024.30'
-Test-Version '2017-latest' '14.0.3460.9'
-Test-Version '2019-latest' '15.0.4298.1'
+#Test-Version '2012-latest' '11.0.7507.2'
+#Test-Version '2014-latest' '12.0.6444.4'
+#Test-Version '2016-latest' '13.0.7024.30'
+#Test-Version '2017-latest' '14.0.3460.9'
+#Test-Version '2019-latest' '15.0.4298.1'
 Test-Version '2022-latest' '16.0.4015.1'
